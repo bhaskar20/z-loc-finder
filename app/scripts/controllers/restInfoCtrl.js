56e025cbd610d0eb6c59ccb6c7ddddc3e45ee35c
bhaskar <sharmabhaskar13@gmail.com>
2016-12-18 19:34:58 +0530

    map fix

--- app/scripts/controllers/restInfoCtrl.js
+++ app/scripts/controllers/restInfoCtrl.js
@@ -4,5 +4,10 @@ angular.module('myApp')
     .controller("restInfoCtrl", restInfoCtrl)
 restInfoCtrl.$inject = ["$scope", "$uibModalInstance", "info"];
 
-function restInfoCtrl($scope, uibModalInstance, info) {
+function restInfoCtrl($scope, $uibModalInstance, info) {
+    $scope.info = info;
+
+    $scope.close = function(){
+        $uibModalInstance.close();
+    }
 }
